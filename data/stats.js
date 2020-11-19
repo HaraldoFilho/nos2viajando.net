cd9a516420ab44e2b7a915f6699c815e980c83e0
Haraldo Filho <haraldo.filho@gmail.com>
2020-11-18 21:13:03 -0300

    Replaced data file 'stats.js' with 'trips.js'

--- data/stats.js
+++
@@ -1,3 +0,0 @@
-var n_trips = 8;
-var distance = 58.065;
-var days = 69;
---
+++ data/trips.js
@@ -0,0 +1,12 @@
+// Year, Trip, Distance (km), Days
+
+var trips = [
+  ['2009', 'Monte Alegre do Sul', 146, 4],
+  ['2012', 'Canela', 2400, 7],
+  ['2016', 'Foz do Iguaçu', 2721, 8],
+  ['2016', 'Analândia', 265, 4],
+  ['2017', 'Califórnia', 24478, 12],
+  ['2018', 'Monte Verde', 313, 3],
+  ['2018', 'Fernando de Noronha', 5463, 9],
+  ['2019', 'Croácia e Itália', 22280, 22]
+]
