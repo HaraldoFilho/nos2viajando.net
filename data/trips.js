26297b05711eaced940f4fc4bb8d3ca47532c513
Haraldo Filho <haraldo.filho@gmail.com>
2021-01-16 10:09:17 -0300

    Adjusted trip data

--- data/trips.js
+++ data/trips.js
@@ -6,7 +6,7 @@ var trips = [
   ['2016', 'Foz do Iguaçu', 2721, 8],
   ['2016', 'Analândia', 265, 4],
   ['2017', 'Califórnia', 24478, 12],
-  ['2018', 'Monte Verde', 313, 3],
+  ['2018', 'Monte Verde', 306, 3],
   ['2018', 'Fernando de Noronha', 5463, 9],
   ['2019', 'Croácia e Itália', 21956, 22]
 ]
@@ -17,7 +17,7 @@ var trips_en = [
   ['2016', 'Foz do Iguaçu', 2721, 8],
   ['2016', 'Analândia', 265, 4],
   ['2017', 'California', 24478, 12],
-  ['2018', 'Monte Verde', 313, 3],
+  ['2018', 'Monte Verde', 306, 3],
   ['2018', 'Fernando de Noronha', 5463, 9],
   ['2019', 'Croatia and Italy', 21956, 22]
 ]
