3debf7f685b7f147cb89f3f10e6804765c3d1269
Haraldo Filho <haraldo.filho@gmail.com>
2022-12-01 19:58:24 -0300

    Moved home variable to a file

--- data/driving.js
+++ data/driving.js
@@ -3,3 +3,17 @@ var driving = [
   ['2016', ['Campinas', 'Chalé Macaúva'], 'Analândia'],
   ['2018', ['Campinas', 'Kuriuwa Hotel'], 'Monte Verde']
 ]
+
+var driving_en = [
+  ['2009', ['Campinas', 'Peixada do Lago', 'Hibiti Hotel Rural'], 'Monte Alegre do Sul'],
+  ['2016', ['Campinas', 'Chalé Macaúva'], 'Analândia'],
+  ['2018', ['Campinas', 'Kuriuwa Hotel'], 'Monte Verde']
+]
+
+var driving_abroad = [
+  ['2017', ['Villa Montes Hotel', 'Hurricane Point', 'Rock Point Restaurant', 'Yosemite Southgate Hotel & Suites'], 'Califórnia']
+]
+
+var driving_abroad_en = [
+  ['2017', ['Villa Montes Hotel', 'Hurricane Point', 'Rock Point Restaurant', 'Yosemite Southgate Hotel & Suites'], 'California']
+]
