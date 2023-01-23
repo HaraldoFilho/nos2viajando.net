976441e3489113a8148579815ebe52bb12186061
Nós Dois Viajando <admin@nos2viajando.net>
2023-01-23 12:51:01 -0300

    Update flights.js

--- data/flights.js
+++ data/flights.js
@@ -4,5 +4,8 @@ var flights = [
   [2017, [['GRU', 'EWR', 'SFO'], ['SFO', 'IAH', 'GRU']]],
   [2018, [['VCP', 'REC', 'FEN'], ['FEN', 'REC', 'VCP']]],
   [2019, [['GRU', 'FCO'], ['FCO', 'SPU', 'ZAG'], ['DBV', 'FCO'], ['NAP', 'FCO', 'GRU']]],
+  // [2023, [['GRU', 'SCL']]]
+  // [2023, [['GRU', 'SCL', 'PUQ']]]
+  // [2023, [['GRU', 'SCL', 'PUQ'], ['PUQ', 'SCL']]]
   // [2023, [['GRU', 'SCL', 'PUQ'], ['PUQ', 'SCL', 'GRU']]]
 ]
