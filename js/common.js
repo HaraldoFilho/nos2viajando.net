8684e837148027a7fc8a54be3e6f97d66cf02f19
Haraldo Filho <haraldo.filho@gmail.com>
2023-09-08 17:51:31 -0300

    Code refactoring

--- js/common.js
+++ js/common.js
@@ -1,5 +1,30 @@
 // Markers
 
+function createMarkers(map, places, color, scale, get_farthest_points) {
+
+  var markers = [];
+
+  for (var i = 0; i < places.length; i++) {
+
+    var text = "<p style=\"text-align:center;margin: 0px 5px -5px 5px;\">" + places[i][2] + "</p>";
+    markers[i] = new mapboxgl.Marker({color:color,scale:scale,draggable:false})
+    .setLngLat(places[i][0])
+    .setPopup(new mapboxgl.Popup({closeButton:false}).setHTML(text));
+
+    if (get_farthest_points) {
+      if (places[i][0][1] > far_north[0][1]) { far_north = places[i]};
+      if (convertLongToFromHome(places[i][0][0]) > convertLongToFromHome(far_east[0][0])) { far_east = places[i]};
+      if (places[i][0][1] < far_south[0][1]) { far_south = places[i]};
+      if (convertLongToFromHome(places[i][0][0]) < convertLongToFromHome(far_west[0][0])) { far_west = places[i]};
+      farthest_points = [far_north, far_east, far_south, far_west];
+    }
+
+  }
+
+  return markers;
+
+}
+
 function addMarkersToMap(map, markers) {
   for (var i = 0; i < markers.length; i++) {
     markers[i].addTo(map);
@@ -27,6 +52,78 @@ function hideMarkers(map, markers) {
   }
 }
 
+function loadMarkersOnMap(map, markers_scale, icons_path) {
+  if (markers_scale > 0.4) {
+    var home_marker = [];
+    var text = "<p style=\"text-align:center;margin: 0px 5px -5px 5px;\">" + home[2] + ", " + countries[home[1]] + "</p>";
+    home_marker.push(createSpecialMarker(map, home[0], text, icons_path.concat('home.svg'), 24));
+    addMarkersToMap(map, home_marker);
+  }
+  restaurants_markers = createMarkers(map, restaurants, '#ff9955', markers_scale, false);
+  airports_markers = createMarkers(map, airports, '#a0a0a0', markers_scale, true);
+  attractions_markers = createMarkers(map, attractions, '#ff8080', markers_scale, false);
+  parks_markers = createMarkers(map, parks, '#55a455', markers_scale, false);
+  cities_markers = createMarkers(map, cities, '#3fb1ce', markers_scale, true);
+  photos_markers = createPhotosMarkers(map, locations_dict);
+  farthest_points_markers = createFarthestPointsMarkers(map, farthest_points, getFarthestDistances(), icons_path);
+  addMarkersToMap(map, airports_markers);
+  addMarkersToMap(map, attractions_markers);
+  addMarkersToMap(map, parks_markers);
+  addMarkersToMap(map, cities_markers);
+}
+
+function createFarthestPointsMarkers(map, values, distances, icons_path) {
+
+  var markers = [];
+  var directions = ['north', 'east', 'south', 'west'];
+
+  for (var i = 0; i < values.length; i++) {
+    var icon_name = icons_path.concat("arrow_");
+    if (i == distances[1]) {
+      icon_name = icon_name + "far_";
+    }
+    icon_name = icon_name + directions[i] + ".svg";
+    var text = "<p style=\"text-align:center;margin: 0px 5px -5px 5px;\">" + values[i][2] + ", " + countries[values[i][1]] + "<br><b>" + distances[0][i] + " km</b></p>";
+    markers[i] = createSpecialMarker(map, values[i][0], text, icon_name, 28);
+
+  }
+
+  return markers;
+
+}
+
+function createPhotoMarker(map, value, icons_path) {
+
+  var marker = document.createElement('div');
+  var img = document.createElement('img');
+  img.setAttribute('src', icons_path.concat('marker_photo.svg'));
+  img.setAttribute('width', '20');
+  img.setAttribute('height', '20');
+  marker.appendChild(img);
+
+  var htmlText = "<div style=\"max-height:410px;overflow:auto;\">";
+
+  for (var i = 0; i < value[1].length; i++) {
+    htmlText = htmlText.concat("<a href=\"").concat("https://www.flickr.com/photos/hpfilho/").concat(value[1][i][0])
+    .concat("/\" target=\"_blank\"><img src=\"").concat(value[1][i][1]).concat("\"/></a> ");
+  }
+  htmlText = htmlText.concat("</div>");
+
+  var photo_marker;
+
+  if (value[1].length <= 35) {
+    photo_marker = new mapboxgl.Marker({element:marker,scale:1,draggable:false})
+    .setLngLat(value[0])
+    .setPopup(new mapboxgl.Popup({closeButton:false,maxWidth:'566px'}).setHTML(htmlText));
+  } else {
+    photo_marker = new mapboxgl.Marker({element:marker,scale:1,draggable:false})
+    .setLngLat(value[0])
+    .setPopup(new mapboxgl.Popup({closeButton:false,maxWidth:'592px'}).setHTML(htmlText));
+  }
+
+  return photo_marker;
+
+}
 
 // Listeners
 
@@ -164,13 +261,13 @@ function enterMapFullWindow(current_bbox, current_coords) {
       inputs[i].onclick = switchLayer;
     }
 
-    loadMarkersOnMap(map_fullwindow, 0.7);
+    loadMarkersOnMap(map_fullwindow, 0.7, icons_path);
 
     var fullmap_countries_panel = document.getElementById('fullmap-countries-panel');
     setSelectorPosition();
 
     for (var country_code in countries) {
-      addIcon(country_code, fullmap_countries_panel);
+      addIcon(country_code, fullmap_countries_panel, icons_path);
       addListenerToFlagsFullWindow(country_code);
     }
 
@@ -291,7 +388,7 @@ function createPhotosMarkers(map, locations) {
   var m = 0;
   for (var country in locations) {
     for (var i = 0; i < locations[country].length; i++) {
-      photos_markers[m] = createPhotoMarker(map, locations[country][i]);
+      photos_markers[m] = createPhotoMarker(map, locations[country][i], icons_path);
       m++;
     }
   }
@@ -865,4 +962,26 @@ function getInterLatitude(coord_1, coord_2) {
   var dist_y_2 = dist_x_2 * sin;
   var inter_y = coord_2[1] + dist_y_2;
   return inter_y;
+}
+
+
+// Icons
+
+function getIconSrc(icons_path, country_code) {
+  return icons_path.concat("flags/").concat(countries_bbox[country_code][0])
+  .replace(/\s/g, "-").toLowerCase().concat(".svg");
+}
+
+function addIcon(country_code, panel, icons_path) {
+  var country_name = countries[country_code];
+  var elem = document.createElement("IMG");
+  elem.setAttribute("id", country_code.concat("__"));
+  elem.setAttribute("class", "icon");
+  elem.setAttribute("src", getIconSrc(icons_path, country_code));
+  elem.setAttribute("title", country_name);
+  elem.setAttribute("alt", country_name);
+  var div_icon = document.createElement("DIV");
+  div_icon.setAttribute("class", "flag-icon");
+  div_icon.appendChild(elem);
+  panel.appendChild(div_icon);
 }
