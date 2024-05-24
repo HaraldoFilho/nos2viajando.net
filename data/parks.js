99e7655653768c2fc17bcb9d934b5ab9bc827fbc
Haraldo Filho <haraldo.filho@gmail.com>
2024-05-24 15:37:17 -0300

    Edited parks.js

--- data/parks.js
+++ data/parks.js
@@ -7,8 +7,8 @@ var parks = [
   [[-32.415536, -3.860330], 'BR', 'P. Nac. Marinho de Fernando de Noronha'],
   [[15.604231, 44.881627], 'HR', 'Parque Nacional dos Lagos Plitvice'],
   [[15.964112, 43.805828], 'HR', 'Parque Nacional Krka'],
-  [[-73.468834, -50.944152], 'CL', 'Parque Nacional Torres del Paine'],
-  [[-74.493696, -49.786685], 'CL', 'Parque Nacional Bernardo O\'Higgins'],
+  [[-73.088056, -51.056123], 'CL', 'Parque Nacional Torres del Paine'],
+  [[-73.124682, -51.431266], 'CL', 'Parque Nacional Bernardo O\'Higgins'],
   [[-73.234177, -50.330562], 'AR', 'Parque Nacional Los Glaciares']
 ]
 
@@ -21,7 +21,7 @@ var parks_en = [
   [[-32.415536, -3.860330], 'BR', 'Fernando de Noronha National Park'],
   [[15.604231, 44.881627], 'HR', 'Plitvice Lakes National Park'],
   [[15.964112, 43.805828], 'HR', 'Krka National Park'],
-  [[-73.468834, -50.944152], 'CL', 'Torres del Paine National Park'],
-  [[-74.493696, -49.786685], 'CL', 'Bernardo O\'Higgins National Park'],
+  [[-73.088056, -51.056123], 'CL', 'Torres del Paine National Park'],
+  [[-73.124682, -51.431266], 'CL', 'Bernardo O\'Higgins National Park'],
   [[-73.234177, -50.330562], 'AR', 'Los Glaciares National Park']
 ]
