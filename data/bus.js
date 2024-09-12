eb4b9b6ff7f5f14857a583b688c1bd37c9c159c7
Haraldo Filho <haraldo.filho@gmail.com>
2024-09-11 21:17:13 -0300

    Changed behavior of map selector

--- data/bus.js
+++ data/bus.js
@@ -1,15 +1,17 @@
 var bus_rides = [
   ['2016', ['Campinas', 'Aeroporto de Viracopos', 'Campinas'], 'Foz do Iguaçu'],
   ['2017', ['Campinas', 'Aeroporto de Guarulhos', 'Campinas'], 'Califórnia'],
+  ['2019', ['Aeroporto de Roma-Fiumicino', 'Roma'], 'Croácia e Itália'],
   ['2019', ['Zagrebe', 'Hotel Grabovac', 'Zadar', 'Šibenik', 'Split'], 'Croácia e Itália'],
   ['2023', ['Aeroporto de Punta Arenas', 'Puerto Natales', 'El Calafate', 'Puerto Natales', 'Punta Arenas'], 'Patagônia'],
-  ['2024', ['Aeroporto de Montevidéu', 'Punta del Este', 'Colonia del Sacramento'], 'Uruguai']
+  ['2024', ['Aeroporto de Montevidéu', 'Punta del Este', 'Montevidéu', 'Colonia del Sacramento'], 'Uruguai']
 ]
 
 var bus_rides_en = [
   ['2016', ['Campinas', 'Viracopos Airport', 'Campinas'], 'Foz do Iguaçu'],
   ['2017', ['Campinas', 'Guarulhos Airport', 'Campinas'], 'California'],
+  ['2019', ['Rome-Fiumicino Airport', 'Rome'], 'Croatia and Italy'],
   ['2019', ['Zagreb', 'Hotel Grabovac', 'Zadar', 'Šibenik', 'Split'], 'Croatia and Italy'],
   ['2023', ['Punta Arenas Airport', 'Puerto Natales', 'El Calafate', 'Puerto Natales', 'Punta Arenas'], 'Patagonia'],
-  ['2024', ['Montevideo Airport', 'Punta del Este', 'Colonia del Sacramento'], 'Uruguay']
+  ['2024', ['Montevideo Airport', 'Punta del Este', 'Montevideo', 'Colonia del Sacramento'], 'Uruguay']
 ]
