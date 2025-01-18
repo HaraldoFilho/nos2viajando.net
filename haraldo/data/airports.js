1c7debb4024e057248d6f62025a2ab1d48cc09f8
Haraldo Filho <haraldo.filho@gmail.com>
2025-01-18 10:44:12 -0300

    Added Houston airport to Haraldo's solo list

--- haraldo/data/airports.js
+++ haraldo/data/airports.js
@@ -7,6 +7,7 @@ var airports_solo = [
   [[-87.899794, 41.976487], 'US', 'Aeroporto de Chicago', 'ORD'],
   [[-48.545479, -27.670034], 'BR', 'Aeroporto de Florianópolis', 'FLN'],
   [[-35.365953, -5.768892], 'BR', 'Aeroporto de Natal', 'NAT'],
+  [[-95.336579, 29.990220], 'US', 'Aeroporto de Houston', 'IAH'],
   [[-97.666288, 30.197508], 'US', 'Aeroporto de Austin', 'AUS'],
   [[-49.172599, -25.532713], 'BR', 'Aeroporto de Curitiba', 'CWB']
 ]
@@ -20,6 +21,7 @@ var airports_solo_en = [
   [[-87.899794, 41.976487], 'US', 'Chicago Airport', 'ORD'],
   [[-48.545479, -27.670034], 'BR', 'Florianópolis Airport', 'FLN'],
   [[-35.365953, -5.768892], 'BR', 'Natal Airport', 'NAT'],
+  [[-95.336579, 29.990220], 'US', 'Houston Airport', 'IAH'],
   [[-97.666288, 30.197508], 'US', 'Austin Airport', 'AUS'],
   [[-49.172599, -25.532713], 'BR', 'Curitiba Airport', 'CWB']
 ]
