6c6685ea24c546907c2a0e04088166ce37af9700
Haraldo Filho <haraldo.filho@gmail.com>
2025-02-20 16:23:03 -0300

    Update bus.js

--- data/bus.js
+++ data/bus.js
@@ -1,17 +1,21 @@
 var bus_rides = [
-  ['2016', ['Campinas', 'Aeroporto de Viracopos', 'Campinas'], 'Foz do Iguaçu'],
-  ['2017', ['Campinas', 'Aeroporto de Guarulhos', 'Campinas'], 'Califórnia'],
   ['2019', ['Aeroporto de Roma-Fiumicino', 'Roma'], 'Croácia e Itália'],
   ['2019', ['Zagrebe', 'Hotel Grabovac', 'Zadar', 'Šibenik', 'Split'], 'Croácia e Itália'],
   ['2023', ['Aeroporto de Punta Arenas', 'Puerto Natales', 'El Calafate', 'Puerto Natales', 'Punta Arenas'], 'Patagônia'],
-  ['2024', ['Aeroporto de Montevidéu', 'Punta del Este', 'Montevidéu', 'Colonia del Sacramento'], 'Uruguai']
+  ['2024', ['Aeroporto de Montevidéu', 'Punta del Este', 'Montevidéu', 'Colonia del Sacramento'], 'Uruguai'],
+  ['2025', ['Puerto Varas', 'Lago Todos los Santos'], 'Chile'],
+  ['2025', ['Peulla', 'Lago Frias'], 'Chile'],
+  ['2025', ['Lago Frias', 'Puerto Blest' ], 'Chile'],
+  ['2025', ['Puerto Mañuelo', 'Bariloche'], 'Chile']
 ]
 
 var bus_rides_en = [
-  ['2016', ['Campinas', 'Viracopos Airport', 'Campinas'], 'Foz do Iguaçu'],
-  ['2017', ['Campinas', 'Guarulhos Airport', 'Campinas'], 'California'],
   ['2019', ['Rome-Fiumicino Airport', 'Rome'], 'Croatia and Italy'],
   ['2019', ['Zagreb', 'Hotel Grabovac', 'Zadar', 'Šibenik', 'Split'], 'Croatia and Italy'],
   ['2023', ['Punta Arenas Airport', 'Puerto Natales', 'El Calafate', 'Puerto Natales', 'Punta Arenas'], 'Patagonia'],
   ['2024', ['Montevideo Airport', 'Punta del Este', 'Montevideo', 'Colonia del Sacramento'], 'Uruguay']
+  ['2025', ['Puerto Varas', 'Todos los Santos Lake'], 'Chile'],
+  ['2025', ['Peulla', 'Frias Lake'], 'Chile'],
+  ['2025', ['Frias Lake', 'Puerto Blest' ], 'Chile'],
+  ['2025', ['Puerto Mañuelo', 'Bariloche'], 'Chile']
 ]
