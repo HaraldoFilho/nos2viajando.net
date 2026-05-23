2b98cfffa7754047b761e3c59d9d29cb417a61ac
Haraldo Filho <haraldo.filho@gmail.com>
2026-05-23 18:09:27 -0300

    Added attraction

--- raquel/data/trips.js
+++ raquel/data/trips.js
@@ -1,9 +1,9 @@
 // Year, Trip, Distance (km), Days, Local(l)|Domestic(d)|International(i)
 
 var trips_solo = [
-  ['2026', 'Cidade do México', 0, 4, 'i']
+  ['2026', 'Cidade do México', 15158, 4, 'i']
 ]
 
 var trips_solo_en = [
-  ['2026', 'Mexico City', 0, 4, 'i']
+  ['2026', 'Mexico City', 15158, 4, 'i']
 ]
